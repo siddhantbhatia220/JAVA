38f0a0aa97b7b1d7708677dba28b073df655aa27
siddhantbhatia220 <siddhantbhatia220@gmail.com>
2026-02-10 09:41:22 +0530

    Added NOTES File

---
+++ src/com/SiddhantBhatia/JAVA_II/BITMASKING/NOTES.java
@@ -0,0 +1,16 @@
+package com.SiddhantBhatia.JAVA_II.BITMASKING;
+
+public class NOTES {
+    public static void main(String[] args) {
+        int a = 0 ^ 0;
+        int b = 0 ^ 1;
+        int c = 1 ^ 0;
+        int d = 1 ^ 1;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.print(d);
+    }
+
+}
