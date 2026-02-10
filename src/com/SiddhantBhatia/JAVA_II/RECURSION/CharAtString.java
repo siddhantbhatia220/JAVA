622f3e685d94d56106156bc12fc89f19f1a2b6eb
siddhantbhatia220 <siddhantbhatia220@gmail.com>
2026-02-10 09:37:05 +0530

    Added CharAtString File

---
+++ src/com/SiddhantBhatia/JAVA_II/RECURSION/CharAtString.java
@@ -0,0 +1,13 @@
+package com.SiddhantBhatia.JAVA_II.RECURSION;
+
+import java.util.*;
+
+public class CharAtString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        String s = sc.nextLine();
+        //System.out.println(s.charAt());
+        System.out.println(s.substring(3,9));
+
+    }
+}
