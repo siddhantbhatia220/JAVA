d558051f89abee4c73864c53d77096de7f5dbcdc
siddhantbhatia220 <siddhantbhatia220@gmail.com>
2026-02-10 10:40:40 +0530

    Added EmployeeClass file (OOPS)

--- src/com/SiddhantBhatia/JAVA_II/OOPS/EmployeeClass.java
+++ src/com/SiddhantBhatia/JAVA_II/OOPS/EmployeeClass.java
@@ -3,10 +3,19 @@ package com.SiddhantBhatia.JAVA_II.OOPS;
 public class EmployeeClass {
     String name ;
     int id;
-    public EmployeeClass(String name , int id){
-
+    double salary;
+    public EmployeeClass(String name , int id , double salary){
+        this.name = name;
+        this.id  = id;
+        this.salary = salary;
+    }
+    void printdetails(){
+        System.out.println("Name : " + name);
+        System.out.println("Id : " + id);
+        System.out.print("salary : " + salary);
     }
     public static void main(String[] args) {
-
+        EmployeeClass e1 = new EmployeeClass("Siddhant", 713 , 100000);
+        e1.printdetails();
     }
 }
