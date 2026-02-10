c9779a908757bafa44d065a23d0d8fe10f489046
siddhantbhatia220 <siddhantbhatia220@gmail.com>
2026-02-10 09:43:36 +0530

    Added ConstructorDemo file (OOPS)

---
+++ src/com/SiddhantBhatia/JAVA_II/OOPS/ConstructorDemo.java
@@ -0,0 +1,13 @@
+package com.SiddhantBhatia.JAVA_II.OOPS;
+
+public class ConstructorDemo {
+    //public ConstructorDemo() {
+        static int x;
+        //System.out.println();
+    //}
+        public static void main(String[] args){
+        ConstructorDemo cb = new ConstructorDemo();
+            System.out.println(cb.x);
+
+       }
+}
