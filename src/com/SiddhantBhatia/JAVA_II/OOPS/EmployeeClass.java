3f4c0d3165541736025f94a1464d06c853c28e65
siddhantbhatia220 <siddhantbhatia220@gmail.com>
2026-02-10 09:44:06 +0530

    Added EmployeeClass file (OOPS)

---
+++ src/com/SiddhantBhatia/JAVA_II/OOPS/EmployeeClass.java
@@ -0,0 +1,12 @@
+package com.SiddhantBhatia.JAVA_II.OOPS;
+
+public class EmployeeClass {
+    String name ;
+    int id;
+    public EmployeeClass(String name , int id){
+
+    }
+    public static void main(String[] args) {
+
+    }
+}
