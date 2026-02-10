e5b6ef7e9811ccde7234a5be1c9b72308e9173a1
siddhantbhatia220 <siddhantbhatia220@gmail.com>
2026-02-10 09:40:40 +0530

    Added SubString File

---
+++ src/com/SiddhantBhatia/JAVA_II/RECURSION/SubString.java
@@ -0,0 +1,11 @@
+package com.SiddhantBhatia.JAVA_II.RECURSION;
+
+import java.util.*;
+
+public class SubString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        String s = sc.nextLine();
+        System.out.println(s.substring(3,9));
+    }
+}
