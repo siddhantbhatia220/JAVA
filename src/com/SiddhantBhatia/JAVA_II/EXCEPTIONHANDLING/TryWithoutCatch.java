2b561818a57d903185ec882c31146c51cd369b5e
siddhantbhatia220 <siddhantbhatia220@gmail.com>
2026-02-11 10:20:49 +0530

    Added TryWithoutCatch.java file (Using try without catch for handling Exception)

---
+++ src/com/SiddhantBhatia/JAVA_II/EXCEPTIONHANDLING/TryWithoutCatch.java
@@ -0,0 +1,12 @@
+package com.SiddhantBhatia.JAVA_II.EXCEPTIONHANDLING;
+
+//public class TryWithoutCatch {
+//    public static void main(String[] args) {
+//        try{
+//            int x = 10/0;
+//        }
+//        System.out.println("Arithmetic Exception");
+//    }
+//}
+
+//try without catch
