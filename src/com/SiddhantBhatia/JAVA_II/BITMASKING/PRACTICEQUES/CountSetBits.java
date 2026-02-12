5cd9efdf5b07d67003496ad73120664516742782
siddhantbhatia220 <siddhantbhatia220@gmail.com>
2026-02-12 10:44:06 +0530

    Added CountSetBits.java file            function to count the number of 1s in the binary representation of a number.

---
+++ src/com/SiddhantBhatia/JAVA_II/BITMASKING/PRACTICEQUES/CountSetBits.java
@@ -0,0 +1,21 @@
+package com.SiddhantBhatia.JAVA_II.BITMASKING.PRACTICEQUES;
+
+import java.util.*;
+
+public class CountSetBits {
+    public static int countsetbits(int n){
+        int count = 0;
+        while ( n > 0) {
+            if ((n & 1) == 1) {
+                count++;
+            }
+            n = n >> 1;
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(countsetbits(n));
+    }
+}
