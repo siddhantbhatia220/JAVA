23d61e4e8f2c3c1797a2336c328b9fa7a8a0f909
siddhantbhatia220 <siddhantbhatia220@gmail.com>
2026-02-12 10:57:58 +0530

    Added FindUniqueElement.java file       In an array where every element appears twice except one, find the unique element.

---
+++ src/com/SiddhantBhatia/JAVA_II/BITMASKING/PRACTICEQUES/FindUniqueElement.java
@@ -0,0 +1,22 @@
+package com.SiddhantBhatia.JAVA_II.BITMASKING.PRACTICEQUES;
+
+import java.util.*;
+
+public class FindUniqueElement {
+    public static void finduniqueele(int n , int [] arr){
+        int ans = 0;
+        for(int i = 0 ; i < n ; i++){
+            ans = ans ^ arr[i];
+        }
+        System.out.println(ans);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0 ; i < n ; i++){
+            arr[i] = sc.nextInt();
+        }
+        finduniqueele(n , arr);
+    }
+}
