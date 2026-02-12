34cdf2bef62b9aeeb42fa2041284e4203ad2f5a5
siddhantbhatia220 <siddhantbhatia220@gmail.com>
2026-02-12 10:43:18 +0530

    Added CheckPowerOfTwo.java file function to check whether a given number is a power of 2 using bit manipulation.

---
+++ src/com/SiddhantBhatia/JAVA_II/BITMASKING/PRACTICEQUES/CheckPowerOfTwo.java
@@ -0,0 +1,15 @@
+package com.SiddhantBhatia.JAVA_II.BITMASKING.PRACTICEQUES;
+
+import java.util.*;
+
+public class CheckPowerOfTwo {
+    public static boolean checkpower(int n){
+        return n>0 && (n&(n-1))==0;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n= sc.nextInt();
+        System.out.println(checkpower(n));
+
+    }
+}
