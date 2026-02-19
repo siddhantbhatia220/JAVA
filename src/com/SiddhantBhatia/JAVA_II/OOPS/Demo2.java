ccd7365ae111b75acda7f88e4600427f88c0ac14
siddhantbhatia220 <siddhantbhatia220@gmail.com>
2026-02-19 09:50:30 +0530

    Added Demo2.java file              with static vs instance variable tests

---
+++ src/com/SiddhantBhatia/JAVA_II/OOPS/Demo2.java
@@ -0,0 +1,30 @@
+package com.SiddhantBhatia.JAVA_II.OOPS;
+
+public class Demo2 {
+//    int rollNo = 10 ;
+    //static int y = 20;
+    int x = 10;
+//    static int count = 20;
+//    public void print(){
+//        System.out.println(rollNo);
+//        rollNo++;
+//        System.out.println(count);
+//        count++;
+//        System.out.println("Y:"+y);
+//        y++;
+    //}
+    void fun(){
+        x= 30;
+    }
+    public static void main(String[] args) {
+//        Demo2 d = new Demo2();
+//        System.out.println(count);
+//        System.out.println(d.rollNo);
+//        Demo2 d1 = new Demo2();
+//        d1.print();
+//        d1.print();
+//        System.out.println(d.rollNo);
+//        d.print();
+        
+    }
+}
